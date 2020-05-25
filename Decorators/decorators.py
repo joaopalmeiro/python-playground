63365fe6c1d1ea570d533ed4009dacd5793a26f4
João Palmeiro <joaommpalmeiro@gmail.com>
2020-05-25 12:09:36 +0100

    Updated: Decorators.

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -24,7 +24,7 @@ def timer(func):
 
         # `!r` - convert the value to a string using repr()
         print(
-            f"\U000023F0 The \033[1m{func.__name__!r}\033[0m function finished in \033[1m{runtime:.4f}\033[0m seconds."
+            f"\n\U000023F0 The \033[1m{func.__name__!r}\033[0m function finished in \033[1m{runtime:.4f}\033[0m seconds."
         )
 
         return value
@@ -32,9 +32,25 @@ def timer(func):
     return wrapper_timer
 
 
+def debug_args(func):
+    @wraps(func)
+    def wrapper_debug_args(*args, **kwargs):
+        args_r = [repr(a) for a in args]
+        kwargs_r = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_r + kwargs_r)
+        print(f"\U0001F919 \033[1m{func.__name__}\033[0m({signature})\n")
+
+        value = func(*args, **kwargs)
+        return value
+
+    return wrapper_debug_args
+
+
 # Decorated function
 @timer
+@debug_args
 def now():
+    time.sleep(2)
     today = date.today().strftime("%d/%m/%Y")
     print(f"Date: {today}")
 
