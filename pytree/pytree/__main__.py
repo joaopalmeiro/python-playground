a7a1c95afc68fac7554624aa36a49bca30737205
João Palmeiro <joaommpalmeiro@gmail.com>
2020-07-16 00:20:37 +0100

    Updated: Logic.

--- pytree/pytree/__main__.py
+++ pytree/pytree/__main__.py
@@ -1,33 +1,55 @@
 # __main__.py
 
-import os
-
-from utils import get_indentation, clean_terminal
-
-ONE_PART = "│"
-TWO_PARTS = "└──"
-THREE_PARTS = "├──"
+from pathlib import Path
+from utils import clean_terminal, tree_wo_indentation_lines
 
 
-def main():
-    cwd = os.getcwd()
+SPACE = " " * 4
+ONE_PART = "│   "
+TWO_PARTS = "└── "
+THREE_PARTS = "├── "
 
-    for dirpath, _, filenames in os.walk(cwd):
-        depth = dirpath.replace(cwd, "", 1).count(os.sep)
 
-        indentation = get_indentation(depth, True)
-        inner_indentation = get_indentation(depth, False)
+def tree_generator(root, pre_indentation_line):
+    """
+    Based on: https://stackoverflow.com/questions/9727673/list-directory-tree-structure-in-python
+    """
+    file_and_dir_names = sorted(root.iterdir())
+    indentation_lines = [THREE_PARTS] * (len(file_and_dir_names) - 1) + [TWO_PARTS]
 
-        bname = os.path.basename(dirpath)
+    # Using `zip` (instead of comparing each element to the last one on a list,
+    # for example) is important to abstract the possible names of files and folders
+    # from which indentation lines they should have.
+    branches = zip(indentation_lines, file_and_dir_names)
 
-        indentation_line = THREE_PARTS if depth > 0 else ""
+    for indentation_line, path in branches:
+        yield pre_indentation_line + indentation_line + path.name
 
-        print(f"{indentation}{indentation_line}{bname}")
+        if path.is_dir():
+            new_pre_indentation_line = (
+                ONE_PART if indentation_line == THREE_PARTS else SPACE
+            )
+            yield from tree_generator(
+                path, pre_indentation_line + new_pre_indentation_line
+            )
 
-        for fn in filenames:
-            print(f"{inner_indentation}{fn}")
+
+def get_root_representation(root, root_representation):
+    if root_representation == "name":
+        return root.name
+    elif root_representation == "dot":
+        return "."
+    else:
+        return ""
+
+
+def main(root, pre_indentation_line, root_representation):
+    print(get_root_representation(root, root_representation))
+    for branch in tree_generator(root, ""):
+        print(branch)
 
 
 if __name__ == "__main__":
     clean_terminal()
-    main()
+    main(Path.cwd(), "", "dot")
+    # tree_wo_indentation_lines()
--- pytree/pytree/utils.py
+++ pytree/pytree/utils.py
@@ -3,9 +3,29 @@ from shutil import get_terminal_size
 import os
 
 
-def get_indentation(depth, is_dir, spaces=3):
+def get_indentation(depth, is_dir, spaces=4):
     depth = depth if (depth == 0 or not is_dir) else depth - 1
-    return " " * spaces * (depth)
+
+    return " " * spaces * depth
+
+
+def tree_wo_indentation_lines():
+    cwd = os.getcwd()
+
+    for dirpath, _, filenames in os.walk(cwd):
+        depth = dirpath.replace(cwd, "", 1).count(os.sep)
+
+        indentation = get_indentation(depth, True)
+        inner_indentation = get_indentation(depth, False)
+
+        bname = os.path.basename(dirpath)
+
+        extra_space = " " if depth > 0 else ""
+
+        print(f"{indentation}{extra_space}{bname}")
+
+        for fn in filenames:
+            print(f"{inner_indentation} {fn}")
 
 
 def clean_terminal():
