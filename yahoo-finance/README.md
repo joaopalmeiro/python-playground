7ec4f0f54905721e8bbe8a5386db09f987882ff4
João Palmeiro <joaommpalmeiro@gmail.com>
2021-01-23 02:06:50 +0000

    Update yfinance

--- yahoo-finance/README.md
+++ yahoo-finance/README.md
@@ -2,4 +2,7 @@
 
 ## References
 
-- [yfinance](https://github.com/ranaroussi/yfinance).
+- [yfinance](https://github.com/ranaroussi/yfinance) (Python package).
+- Ran Aroussi's "[Reliably download historical market data from Yahoo! Finance with Python](https://aroussi.com/post/python-yahoo-finance)" blog post.
+- Ryder Brooks's [answer](https://stackoverflow.com/a/47505102) on Stack Overflow.
+- Vicki Boykis' "[A deep dive on Python type hints](http://veekaybee.github.io/2019/07/08/python-type-hints/)" blog post.
--- yahoo-finance/download.py
+++ yahoo-finance/download.py
@@ -1,7 +1,8 @@
 import json
 import time
-from typing import Dict, Union
+from typing import Any, Dict, Union
 
+import pandas as pd
 import requests
 
 BASE_URL = "https://query1.finance.yahoo.com/v8/finance/chart/"
@@ -11,12 +12,13 @@ def jprint(obj, indent=4, ensure_ascii=False):
     print(json.dumps(obj, indent=indent, ensure_ascii=ensure_ascii))
 
 
-def historical_prices(
+def get_historical_prices(
     symbol: str,
     start: str = "2010-01-01",
     end: str = "2021-01-01",
     date_fmt: str = "%Y-%m-%d",
-    interval: str = "1d",
+    granularity: str = "1d",
+    verbose: bool = False,
 ):
     start_unix_secs = int(time.mktime(time.strptime(start, date_fmt)))
     end_unix_secs = int(time.mktime(time.strptime(end, date_fmt)))
@@ -24,15 +26,24 @@ def historical_prices(
     params: Dict[str, Union[int, str]] = {
         "period1": start_unix_secs,
         "period2": end_unix_secs,
-        "interval": interval,
+        "interval": granularity,
     }
 
     url = f"{BASE_URL}{symbol}"
 
     data = requests.get(url=url, params=params)
 
+    if verbose:
+        print("URL:", data.url)
+
     return data.json()
 
 
+def parse_historical_prices(data: Dict[str, Any]) -> pd.DataFrame:
+    pass
+
+
 if __name__ == "__main__":
-    jprint(historical_prices("MSFT", start="2019-04-15", end="2019-04-17"))
+    raw_data = get_historical_prices(
+        "MSFT", start="2019-04-15", end="2019-04-17", verbose=True
+    )
