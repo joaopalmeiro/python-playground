be34c3bbebf3fee3135ca28f19f8981d6422ae22
João Palmeiro <joaommpalmeiro@gmail.com>
2021-04-11 21:39:00 +0100

    Add CLI with input

--- Imaging/README.md
+++ Imaging/README.md
@@ -4,3 +4,11 @@
 
 - `pipenv install --python 3.7`
 - `pipenv shell`
+
+## Notes
+
+- [Image file formats](https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html).
+- [Generate square or circular thumbnail images with Python, Pillow](https://note.nkmk.me/en/python-pillow-square-circle-thumbnail/).
+- [docopt](http://docopt.org/):
+  - [Examples](https://github.com/docopt/docopt/tree/master/examples).
+  - [Repo](https://github.com/docopt/docopt).
--- Imaging/cropify.py
+++ Imaging/cropify.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+# https://github.com/docopt/docopt/blob/master/examples/naval_fate.py
+# https://github.com/docopt/docopt/blob/master/examples/validation_example.py
+# https://github.com/docopt/docopt/blob/master/examples/options_shortcut_example.py
+"""A Python CLI to crop a square from a rectangular image.
+
+Usage:
+  cropify.py IMG
+  cropify.py -h | --help
+  cropify.py -v | --version
+
+Arguments:
+  IMG           path to the image to crop
+
+Options:
+  -h --help     show this help message and exit
+  -v --version  show version and exit
+
+"""
+
 __author__ = "João Palmeiro"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+from docopt import docopt
 from PIL import Image
 
 
@@ -25,11 +45,14 @@ def crop_max_square(img: Image.Image) -> Image.Image:
     return crop_center(img, size, size)
 
 
-def main() -> None:
-    img = Image.open("joao.jpg")
+def main(input_path: str) -> None:
+    img = Image.open(input_path)
     cropped_img = crop_max_square(img)
     cropped_img.save("squared_joao.jpg", quality=95)
 
 
 if __name__ == "__main__":
-    main()
+    args = docopt(__doc__, version=__version__)
+    # print(args)
+
+    main(args["IMG"])
