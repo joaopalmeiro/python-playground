72e1166bc5ed3450452988c2e0148cccb3ef55e9
João Palmeiro <joaommpalmeiro@gmail.com>
2024-08-17 16:40:16 +0100

    Closes #25

--- terminals/cheese.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    # https://en.wikipedia.org/wiki/Say_cheese
-
-    # `cp cheese.py ~` + `python cheese.py`
-    # Use Rectangle (https://rectangleapp.com/)
-    # to scale the terminal window to "Top Center Sixth"
-
-    print("✨ 📸 ✨")
